03e6f7f444a88ee03cb1d4b0eec21b23137c5078
kosich <kosich@gmail.com>
2015-08-30 21:10:16 +0300

    displayed a circle

--- fe/dev/main.js
+++ fe/dev/main.js
@@ -1,51 +1,35 @@
 console.log('FUCK YEAH!!!');
 
+var data = [
+    {
+        name: 'hellow',
+        r: 50,
+        x: 200,
+        y: 100,
+        color: 1
+    }
+];
+
 var width = 960,
     height = 500;
 
 var color = d3.scale.category20();
 
-var force = d3.layout.force()
-    .charge(-120)
-    .linkDistance(30)
-    .size([width, height]);
-
-var svg = d3.select('body').append('svg')
+var svg = d3.select('body')
+    .append('svg')
     .attr('width', width)
     .attr('height', height);
 
-d3.json('data.json', function(error, graph) {
-  if (error) throw error;
+var node = svg
+    .selectAll('.node')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('class', 'node')
+    .attr('cx', function(d){ return d.x; })
+    .attr('cy', function(d){ return d.y; })
+    .attr('r', function(d){ return d.r; })
+    .style('fill', function(d) { return color(d.color); });
 
-  force
-      .nodes(graph.nodes)
-      .links(graph.links)
-      .start();
-
-  var link = svg.selectAll('.link')
-      .data(graph.links)
-    .enter().append('line')
-      .attr('class', 'link')
-      .style('stroke-width', function(d) { return Math.sqrt(d.value); });
-
-  var node = svg.selectAll('.node')
-      .data(graph.nodes)
-    .enter().append('circle')
-      .attr('class', 'node')
-      .attr('r', 5)
-      .style('fill', function(d) { return color(d.group); })
-      .call(force.drag);
-
-  node.append('title')
-      .text(function(d) { return d.name; });
-
-  force.on('tick', function() {
-    link.attr('x1', function(d) { return d.source.x; })
-        .attr('y1', function(d) { return d.source.y; })
-        .attr('x2', function(d) { return d.target.x; })
-        .attr('y2', function(d) { return d.target.y; });
-
-    node.attr('cx', function(d) { return d.x; })
-        .attr('cy', function(d) { return d.y; });
-  });
-});
+node.append('title')
+    .text(function(d) { return d.name; });
