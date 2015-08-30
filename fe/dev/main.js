0e8d0ac52b1f3642ae94a244b0c261d1aafc4729
kosich <kosich@gmail.com>
2015-08-30 23:49:42 +0300

    line between cicles

--- fe/dev/main.js
+++ fe/dev/main.js
@@ -1,12 +1,24 @@
 console.log('FUCK YEAH!!!');
 
-var data = [
-    {
-        x: 200,
-        y: 100,
-        title: 'un title'
-    }
-];
+var data = {
+    nodes: [
+        {
+            id: 0,
+            x: 100,
+            y: 300,
+            title: 'duos titildo'
+        },
+        {
+            id: 1,
+            x: 200,
+            y: 100,
+            title: 'un title'
+        }
+    ],
+    links: [
+        { x1: 100, y1: 300, x2: 200, y2: 100 }
+    ]
+};
 
 var width = 960,
     height = 500;
@@ -17,9 +29,20 @@ var svg = d3
     .attr('width', width)
     .attr('height', height);
 
+var links = svg
+    .selectAll(".link")
+    .data(data.links)
+    .enter()
+    .append("line")
+    .attr("class", "link")
+    .attr('x1', function(d){ return d.x1; })
+    .attr('y1', function(d){ return d.y1; })
+    .attr('x2', function(d){ return d.x2; })
+    .attr('y2', function(d){ return d.y2; });
+
 var node = svg
     .selectAll('.node')
-    .data(data)
+    .data(data.nodes)
     .enter()
     .append('g')
     .attr('class', 'node')
