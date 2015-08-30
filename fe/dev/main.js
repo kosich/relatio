3748221fefc13e0e1ba86658c198d6f075ba64fb
kosich <kosich@gmail.com>
2015-08-30 22:43:39 +0300

    displaying image

--- fe/dev/main.js
+++ fe/dev/main.js
@@ -2,20 +2,16 @@ console.log('FUCK YEAH!!!');
 
 var data = [
     {
-        name: 'hellow',
-        r: 50,
         x: 200,
-        y: 100,
-        color: 1
+        y: 100
     }
 ];
 
 var width = 960,
     height = 500;
 
-var color = d3.scale.category20();
-
-var svg = d3.select('body')
+var svg = d3
+    .select('body')
     .append('svg')
     .attr('width', width)
     .attr('height', height);
@@ -24,12 +20,14 @@ var node = svg
     .selectAll('.node')
     .data(data)
     .enter()
-    .append('circle')
-    .attr('class', 'node')
-    .attr('cx', function(d){ return d.x; })
-    .attr('cy', function(d){ return d.y; })
-    .attr('r', function(d){ return d.r; })
-    .style('fill', function(d) { return color(d.color); });
+    .append('g')
+    .attr('class', 'node');
 
-node.append('title')
-    .text(function(d) { return d.name; });
+node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")";  });
+
+node.append('image')
+    .attr('xlink:href', 'https://github.com/favicon.ico')
+    .attr('x', -8)
+    .attr('y', -8)
+    .attr('width', 16)
+    .attr('height', 16);
