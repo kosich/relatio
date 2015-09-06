bc3be1f119f03dd3a918d826d43f58baf956b480
kosich <kosich@gmail.com>
2015-09-06 11:57:56 +0300

    added basic time indification

--- fe/dev/main.js
+++ fe/dev/main.js
@@ -1,4 +1,5 @@
-console.log('FUCK YEAH!!!');
+var STROKE_WIDTH = 2;
+
 
 var App = {};
 
@@ -15,7 +16,7 @@ App.CurrentUserView = Backbone.View.extend({
     },
 
     render: function(){
-        this.$el.html( '<h1>' + this.model.get('title') + '</h1>' );
+        this.$el.html( '<h1 class="page-title">' + this.model.get('title') + '</h1>' );
 
         var links = this.model.get('links');
         var nodes = this.model.get('nodes');
@@ -24,39 +25,10 @@ App.CurrentUserView = Backbone.View.extend({
             height = 500;
 
         var svg = d3
-            .select('body')
+            .select('#main')
             .append('svg')
             .attr('width', width)
             .attr('height', height);
-
-        var node = svg
-            .selectAll('.node')
-            .data(nodes)
-            .enter()
-            .append('g')
-            .attr('class', 'node')
-            .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')';  });
-
-        node.append('text')
-            .text(function(d){ return d.title; })
-            .attr('class', 'title')
-            .attr('dx', 11)
-            .attr('dy', '.35em');
-
-        node.append("foreignObject")
-            .attr("width", 480)
-            .attr("height", 500)
-            .attr('dx', 11)
-            .attr('dy', '5em')
-            .append("xhtml:div")
-            .html(function(d){ return d.desc; });
-
-        node.append('image')
-            .attr('xlink:href', 'https://github.com/favicon.ico')
-            .attr('x', -8)
-            .attr('y', -8)
-            .attr('width', 16)
-            .attr('height', 16);
 
         var links = svg
             .selectAll('.link')
@@ -64,6 +36,7 @@ App.CurrentUserView = Backbone.View.extend({
             .enter()
             .append('line')
             .attr('class', 'link')
+            .attr('stroke-width', function(d){ return d.weight * STROKE_WIDTH })
             .attr('x1', function(d){ return getCoordinatesOfNode(d.a).x; })
             .attr('y1', function(d){ return getCoordinatesOfNode(d.a).y; })
             .attr('x2', function(d){ return getCoordinatesOfNode(d.o).x; })
@@ -80,6 +53,41 @@ App.CurrentUserView = Backbone.View.extend({
             
             return found[0];
         }
+        var node = svg
+            .selectAll('.node')
+            .data(nodes)
+            .enter()
+            .append('g')
+            .attr('class', 'node')
+            .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')';  });
+
+        node.append('text')
+            .text(function(d){ return d.title; })
+            .attr('class', 'title')
+            .attr('dx', 40);
+
+        node.append('text')
+            .text(function(d){ return d.years; })
+            .attr('class', 'years')
+            .attr('dx', 40)
+            .attr('dy', '1.1em');
+
+        node.append("foreignObject")
+            .attr('x', 40)
+            .attr('y', '1em')
+            .attr("width", 480)
+            .attr("height", 500)
+            .append("xhtml:div")
+            .attr('class', 'desc')
+            .html(function(d){ return d.desc; });
+
+        node.append('image')
+            .attr('xlink:href', function(d){ return d.imgUrl; })
+            .attr('x', -32)
+            .attr('y', -32)
+            .attr('width', 64)
+            .attr('height', 64);
+
 
     }
 });
